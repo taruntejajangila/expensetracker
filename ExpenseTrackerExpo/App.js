df53ca49739ac8d1aa9ec6a8e0ba6707fd55ab61
Tarun Teja <taruntejajangilayt@gmail.com>
2025-10-26 15:37:12 +0530

    Remove Settings from bottom tab bar as screen is empty

--- ExpenseTrackerExpo/App.js
+++ ExpenseTrackerExpo/App.js
@@ -103,9 +103,11 @@ function TabNavigator() {
             iconName = focused ? 'home' : 'home-outline';
           } else if (route.name === 'Profile') {
             iconName = focused ? 'person' : 'person-outline';
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'settings' : 'settings-outline';
           }
+          // Settings removed from bottom tab bar
+          // else if (route.name === 'Settings') {
+          //   iconName = focused ? 'settings' : 'settings-outline';
+          // }
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
@@ -139,7 +141,8 @@ function TabNavigator() {
     >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      {/* Settings hidden as screen is empty */}
+      {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
     </Tab.Navigator>
   );
 }
